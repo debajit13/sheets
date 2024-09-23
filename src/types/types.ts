c50fead7a287932cd578cf06dbead60cf6a680cb
debajit13 <mallickdebajit3@gmail.com>
2024-09-23 16:48:09 +0530

    add data to Sheets and refactor the codebase to make it modular

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export interface Change {
+  x: number;
+  y: number;
+  value: string;
+}
+
+export interface VisibleResult {
+  visible: number[];
+  start: number[];
+  end: number[];
+}
+
+export interface CoordinateType {
+  x: number;
+  y: number;
+}
